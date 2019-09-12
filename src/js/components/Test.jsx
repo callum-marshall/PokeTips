19ce44d6fa510317b60b4fe7a6c1fa2a459c5e47
callum-marshall <callum_marshall@hotmail.co.uk>
2019-09-12 17:51:13 +0100

    add pokedex-promise
    Co-authored-by: Oliver Cripps <ollycripps@gmail.com>

--- src/js/components/Test.jsx
+++ src/js/components/Test.jsx
@@ -1,18 +1,35 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
 
-const times = 100
+import Pokedex from 'pokedex-promise-v2'
 
-const test = <li> TEST </li>
+let P = new Pokedex()
 
-let testArray = []
+// const times = 100
+//
+// const test = <li> TEST </li>
+//
+// let testArray = []
+//
+// for(var i=0; i < times; i++){
+//     testArray.push(test)
+// }
+//
+// const testJSX = <ul>{testArray}</ul>
+//
+// const wrapper = document.getElementById("test-element");
+//
+// ReactDOM.render(testJSX, wrapper)
 
-for(var i=0; i < times; i++){
-    testArray.push(test)
-}
+const wrapper = document.getElementById("test-element")
 
-const testJSX = <ul>{testArray}</ul>
-
-const wrapper = document.getElementById("test-element");
-
-ReactDOM.render(testJSX, wrapper)
+P.getPokemonByName('eevee')
+  .then(function(response) {
+    console.log(response)
+    let pokemonInfo = [response.name, response['types']['0']['type']['name']]
+    ReactDOM.render(pokemonInfo, wrapper)
+  })
+  .catch(function(error) {
+    console.log('There was an ERROR: ', error)
+    ReactDOM.render("ERROR", wrapper)
+  });
