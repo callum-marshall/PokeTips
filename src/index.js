607408833bbb7febc062410ff4f86bebb183ab3a
Oliver Cripps <ollycripps@gmail.com>
2019-09-13 15:47:10 +0100

    add TextInput component and css-loader
    Co-authored-by: Callum Marshall <callum_marshall@hotmail.co.uk>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom')
 import "./js/components/Name.jsx"
 import "./js/components/Type-1.jsx"
 import "./js/components/Type-2.jsx"
+import css from "./css/styles.css"
 
 ReactDOM.render(
   <App />,
---
+++ src/js/components/TextInput.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class TextInput extends Component {
+  render() {
+    return (
+      <div className="field" />
+    )
+  }
+}
